a26bbf5a3249d37e83c6644f68de173b6d2ccbdd
jsgarrett22 <jgarrett35@student.cscc.edu>
2020-10-23 13:32:09 -0400

    Initial Commit

---
+++ src/edu/cscc/RPSLSpock.java
@@ -0,0 +1,75 @@
+package edu.cscc;
+import java.util.Random;
+
+/**
+ * Class used to generate, validate and calculate winning condition based on computer and human picks
+ * @author Joshua S. Garrett
+ */
+public class RPSLSpock {
+
+    static Random rand = new Random(System.currentTimeMillis());
+
+    public static final String ROCK = "rock";
+    public static final String PAPER = "paper";
+    public static final String SCISSORS = "scissors";
+    public static final String LIZARD = "lizard";
+    public static final String SPOCK = "spock";
+
+    /**
+     * Validates whether a pick is 'Rock', 'Paper', 'Scissors', 'Lizard' or 'Spock'
+     * @param pick computer or human pick
+     * @return a boolean depicting if the passed in pick is 'Rock', 'Paper', 'Scissors', 'Lizard' or 'Spock'
+     */
+    public static boolean isValidPick(String pick) {
+        if (pick == null) {
+            return false;
+        }
+        pick = pick.trim();
+        return (ROCK.equalsIgnoreCase(pick) ||
+                PAPER.equalsIgnoreCase(pick) ||
+                SCISSORS.equalsIgnoreCase(pick) ||
+                LIZARD.equalsIgnoreCase(pick) ||
+                SPOCK.equalsIgnoreCase(pick));
+    }
+
+    /**
+     * Generates a random pick for the computer
+     * @return a random String value in the form of 'Rock', 'Paper', 'Scissors', 'Lizard', or 'Spock'
+     */
+    public static String generatePick() {
+        String pick = null;
+        switch (rand.nextInt(5)) {
+            case 0:
+                pick = ROCK;
+                break;
+            case 1:
+                pick = PAPER;
+                break;
+            case 2:
+                pick = SCISSORS;
+                break;
+            case 3:
+                pick = LIZARD;
+                break;
+            case 4:
+                pick = SPOCK;
+                break;
+        }
+        return pick;
+    }
+
+    /**
+     * Calculates whether the computer or human wins based on picks
+     * @param c_pick computer pick
+     * @param h_pick human pick
+     * @return a boolean value based on the condition of both human and computer pick values
+     */
+    public static boolean isComputerWin(String c_pick,String h_pick) {
+        h_pick = h_pick.toLowerCase();
+        return ((ROCK.equals(c_pick) && (SCISSORS.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (PAPER.equals(c_pick) && (ROCK.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SCISSORS.equals(c_pick) && (PAPER.equals(h_pick) || LIZARD.equals(h_pick))) ||
+                (LIZARD.equals(c_pick) && (PAPER.equals(h_pick) || SPOCK.equals(h_pick))) ||
+                (SPOCK.equals(c_pick) && (ROCK.equals(h_pick) || SCISSORS.equals(h_pick))));
+    }
+}
